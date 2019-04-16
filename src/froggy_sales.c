b8ae59aad403b8e5629be5cc6258957206ebeff2
buildYourNet <i.strimmenos@lenis.gr>
2019-04-17 01:36:30 +0300

    We see light in the horizon

--- src/froggy_sales.c
+++ src/froggy_sales.c
@@ -19,6 +19,12 @@
 #define FLAG_LOCK 2
 #define FLAG_UNLOCK 3
 
+#define SEAT_EMPTY 0
+#define SEAT_OCCUPIED 3
+
+#define SUCCESS 0
+#define FAIL 1
+
 int n_cust;
 unsigned int seed;
 
@@ -30,46 +36,58 @@ int sum_transaction_time = 0;
 int sum_waiting_time = 0;
 
 int theater_seats [N_SEAT] = {0};
+int available_seats = N_SEAT;
 int seats_threshold = 0;
 
 pthread_mutex_t operators;
 pthread_mutex_t expression_of_interest;
+pthread_mutex_t payment;
+pthread_mutex_t seats_availability;
 
 
 typedef struct transaction_info{
     int transaction_no;
     int* seats[N_SEATHIGH];
+    int requested_seats;
     int cost;
 }TRASACTION_INFO;
 
 void mutex_handle(pthread_mutex_t* mutex, int flag){
     int rc;
-    if (flag == 0) {//mutex init
+    if (flag == FLAG_INIT) {
         rc = pthread_mutex_init(mutex, NULL);
 	    if (rc != 0) {
             printf("E R R O R ! ! !\nThe core feels that darkness is strong in you...\nPromblem at mutex init.");
 		    exit(-1);
         }
-	}else if (flag == 1){//mutex destroy
+	}else if (flag == FLAG_DESTROY){
         rc = pthread_mutex_destroy(&operators);
 	    if (rc != 0) {
             printf("E R R O R ! ! !\nThe core feels that darkness is strong in you...\nThe mutex survived.");
       	    exit(-1);
         }
-    }else if (flag == 2){//mutex lock
+    }else if (flag == FLAG_LOCK){
         rc = pthread_mutex_lock (mutex);
         if (rc != 0) {	
             printf("E R R O R ! ! !\nThe core feels that darkness is strong in you...\nThe rise of the Mutex.");
             pthread_exit(&rc);
         }
-    }else{//mutex unlock
+    }else if (flag == FLAG_UNLOCK){
         rc = pthread_mutex_unlock(mutex);
         if (rc != 0) {	
             printf("E R R O R ! ! !\nThe core feels that darkness is strong in you...\nThe fall of the Mutex.");
             pthread_exit(&rc);
         }
     }
-} 
+}
+
+int rand_prob(float percent, int min, int max){
+    int r  = rand();
+    if(r < RAND_MAX * percent)
+        return SUCCESS;
+    else
+        return FAIL;
+}
 
 void wait_operator(TRASACTION_INFO* info) {
     mutex_handle(&operators, FLAG_LOCK);
@@ -85,10 +103,10 @@ void wait_operator(TRASACTION_INFO* info) {
     mutex_handle(&operators, FLAG_UNLOCK);
 }
 
-int request_seats_from_client(){
+int request_seats(int* clientID){
     int seats;
 
-    printf("Welcome to Lucas Film Theater! \nYou can pick from #%d to #%d. \nHow many seats do you want? ", N_SEATLOW, N_SEATHIGH);
+    printf("\nCLient #%d:: Welcome to Lucas Film Theater! \nYou can pick from #%d to #%d. \nHow many seats do you want? ", *clientID, N_SEATLOW, N_SEATHIGH);
     seats = rand_r(&seed) % (N_SEATHIGH + 1);
     if(seats < N_SEATLOW)
         seats = N_SEATLOW;
@@ -96,52 +114,130 @@ int request_seats_from_client(){
     return seats;
 }
 
-void find_seats (int number_of_seats, TRASACTION_INFO* info){
+int find_seats (TRASACTION_INFO* info){
+    sleep(2); //pretend operator is looking
     int i;
     int j = 0;
     int new_threshold = seats_threshold;
     for (i = seats_threshold; i < N_SEAT; i ++){
         /*if seat == 0 (empty), mark it as occupied*/
-        if(!theater_seats[i]){
+        if(theater_seats[i] == SEAT_EMPTY){
             mutex_handle(&expression_of_interest, FLAG_LOCK);    
-            theater_seats[i] = -1;      // -1 = occupied
+            if(theater_seats[i] != 0)
+                continue;
+            theater_seats[i] = SEAT_OCCUPIED;
             info->seats[j++] = i;
             mutex_handle(&expression_of_interest, FLAG_UNLOCK);
         /*if all the previous seats are sold, we have a new threshold*/
-        }else if(theater_seats[i] > 0){
+        }
+        else if(theater_seats[i] > 0){
             if (new_threshold == i-1){
                 new_threshold = i;
             }
-        if (j == number_of_seats){
+        }
+        if (j == info->requested_seats){
             break;
         }
     }
-    if (j < number_of_seats){
-        /* not enough seats available */
+    if (j < info->requested_seats){
+        /*If not enough seats are found then free those which are found*/
+        mutex_handle(&expression_of_interest, FLAG_LOCK); 
+        for(i = 0; i < j; i ++){
+            theater_seats[*info->seats[i]] = SEAT_EMPTY;
+        }
+        mutex_handle(&expression_of_interest, FLAG_UNLOCK);
+        return FAIL;
     }
-    seats_threshold = new_threshold;   
-    //should we dump the array?
+    seats_threshold = new_threshold;
+    return SUCCESS;
+}
+
+int pay_seats(int amount) {
+    int res = rand_prob(0.9F, 0, 1);
+    if(res == SUCCESS){
+        mutex_handle(&payment, FLAG_LOCK);
+        account_remain += amount;
+        mutex_handle(&payment, FLAG_UNLOCK);
+        return SUCCESS;
+    }
+    return FAIL;
+}
+
+void change_seats_state (int new_state, TRASACTION_INFO* info){
+    mutex_handle(&expression_of_interest, FLAG_LOCK);
+    for(int i = 0; i < info->requested_seats; i++)
+    {
+        theater_seats[*info->seats[i]] = new_state;
+    }
+    mutex_handle(&expression_of_interest, FLAG_LOCK);
+}
+
+void change_seats_availability (TRASACTION_INFO* info){
+    mutex_handle(&available_seats, FLAG_LOCK);
+    available_seats -= info->requested_seats;
+    mutex_handle(&available_seats, FLAG_LOCK);
 }
 
 void* transaction (void* clientID){
+    int res, i;
     int* tid = (int*) clientID;
-    printf("Client #%d just called. \n", *tid);
+    printf("\nClient #%d just called. \n", *tid);
 
     TRASACTION_INFO* info;
 
-    struct timespec req_start, req_end;
-    clock_gettime( CLOCK_REALTIME, &req_start);
+    struct timespec req_start, req_end, trans_end;
+    
     clock_gettime( 0, &req_start);
     
     //Await available operator
     wait_operator(info);
-    int requested_seats = request_seats_from_client();
-    printf("So you want #%d seats..hmm let me check..", requested_seats);
+
+    clock_gettime( 0, &req_end);
+
+    if(available_seats == 0){
+        printf("\nClient #%d:: Sorry but the theater is full...", *tid);
+    }
+    else{
+        info->requested_seats = request_seats(tid);
+        printf("\nClient #%d:: So you want #%d seats..hmm let me check..", *tid, info->requested_seats);
+        
+        //check seats availability in theater
+        res = find_seats(info);
+        if(res == SUCCESS){
+            printf("\nClient #%d:: We successfully found the following seats:", *tid);
+            for(i = 0; i < info->requested_seats; i++)
+            {
+                printf("\nClient #%d:: No. %d. ", *tid, *info->seats[i]);
+            }
+
+            printf("\nClient #%d:: Transaction Cost: %d", *tid, info->cost);
+            info->cost = info->requested_seats * C_SEAT;
+            res = pay_seats(info->cost);
+            if(res == SUCCESS){
+                change_seats_state(*tid, info);
+
+                printf("\nClient #%d:: You booked %d seats successfull! Enjoy the play!", *tid, info->requested_seats);
+                printf("\nClient #%d:: Transaction ID: %d", *tid, info->transaction_no);
+                printf("\nClient #%d:: Amount Paid: %d", *tid, info->cost);
+                for(i = 0; i < info->requested_seats; i++)
+                {
+                    printf("\nClient #%d:: Booked Seat No. %d", *tid, *info->seats[i]);
+                }
+            }
+            else {
+                change_seats_state(SEAT_EMPTY, info);
+                printf("\nClient #%d:: Sorry, your payment wasn't successfull. Your booking is canceled", *tid);
+            }
+        }
+        else {
+            printf("\nClient #%d:: Sorry can't proceed with your booking because theater doesn't have enough seats", *tid);
+        }
+    }
     
-    //check seats availability in theater
-    find_seats(requested_seats, info);
-    //pay
-    //update mean values    
+    clock_gettime( 0, &trans_end);
+    //update mean values
+    sum_waiting_time += req_end.tv_sec - req_start.tv_sec;
+    sum_transaction_time += trans_end.tv_sec - req_start.tv_sec;
 
     pthread_exit (clientID);
 }
@@ -176,7 +272,9 @@ int main (int argc, char* argv[]){
 
     //mutex initialization
     mutex_handle(&operators, FLAG_INIT);
-    mutex_handle(&theater, FLAG_INIT);
+    mutex_handle(&expression_of_interest, FLAG_INIT);
+    mutex_handle(&payment, FLAG_INIT);
+    mutex_handle(&seats_availability, FLAG_INIT);
 
     //thread loop
     int i;
@@ -200,6 +298,29 @@ int main (int argc, char* argv[]){
             exit(-1);
         }
     }
+
+    double mean_waiting_time = sum_waiting_time / n_cust;
+    double mean_transaction_time = sum_transaction_time / n_cust;
+
+    printf("\n\nSolding tickets ended!");
+    printf("\nMean waiting time: %lf seconds", mean_waiting_time);
+    printf("\nMean transactions time: %lf seconds", mean_transaction_time);
+    printf("\n\nSeats plan:");
+    for(int i = 0; i < N_SEAT; i++)
+    {
+        if(theater_seats[i] != SEAT_EMPTY)
+            printf("\n --Seat No. %d / Client %d", i + 1, theater_seats[i]);
+        else
+            printf("\n --Seat No. %d / Empty", i + 1);            
+    }
+    
+
+    //remove memory leaks
+    realloc(clients, 0);
+    mutex_handle(&operators, FLAG_DESTROY);
+    mutex_handle(&expression_of_interest, FLAG_DESTROY);
+    mutex_handle(&payment, FLAG_DESTROY);
+    mutex_handle(&seats_availability, FLAG_DESTROY);
 
 
     return 0;
